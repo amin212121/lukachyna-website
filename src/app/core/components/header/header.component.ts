034741b2a39fe03513687d5249ded514eaf12766
Stepan <lukachina.stepan@gmail.com>
2025-01-03 22:35:26 +0200

    initial commit

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ly-header-component',
+  templateUrl: './header.component.html',
+  standalone: true
+})
+
+export class HeaderComponent {}
